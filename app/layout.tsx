934d9fe329c58be7fb5d150637fe29e3bccfdbd5
wojinping-alt <wojinping@gmail.com>
2026-05-31 20:57:27 +0800

    Inline redirect HTTP visits to HTTPS

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,19 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                var host = window.location.host;
+                if (window.location.protocol === 'http:' || host === 'zishoo.cn') {
+                  var nextHost = host === 'zishoo.cn' ? 'www.zishoo.cn' : host;
+                  window.location.replace('https://' + nextHost + window.location.pathname + window.location.search + window.location.hash);
+                }
+              })();
+            `,
+          }}
+        />
         <CanonicalRedirect />
         <Navbar />
 
